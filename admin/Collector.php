0ca7f3805450631aab506d964014a563b671d1f3
jguillin <jguillin@espol.edu.ec>
2018-02-16 15:43:53 -0500

    mandalo2

--- admin/Collector.php
+++ admin/Collector.php
@@ -5,16 +5,16 @@ include_once('dataBase.php');
 // Define configuration
 
 //HEROKU DATABASE
-//define("DB_HOST", "ec2-54-221-198-206.compute-1.amazonaws.com");
-//define("DB_USER", "nlvmxgufjdyvef");
-//define("DB_PASS", "6d4638c7274c4e92f361b9d7854651256413f88a6a21dcd76781c02fb8809b2d");
-//define("DB_NAME", "die4odlfjih80");
+define("DB_HOST", "ec2-54-221-198-206.compute-1.amazonaws.com");
+define("DB_USER", "nlvmxgufjdyvef");
+define("DB_PASS", "6d4638c7274c4e92f361b9d7854651256413f88a6a21dcd76781c02fb8809b2d");
+define("DB_NAME", "die4odlfjih80");
 
 //LOCAL DATABASE
-   define("DB_HOST", "localhost");
-   define("DB_USER", "postgres");
-   define("DB_PASS", "usuario");
-   define("DB_NAME", "Bdatos");
+  // define("DB_HOST", "localhost");
+   //define("DB_USER", "postgres");
+   //define("DB_PASS", "usuario");
+   //define("DB_NAME", "Bdatos");
 
 
 class Collector extends dataBase
