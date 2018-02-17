bbfaf4784ce72f68003e94a5bfa89b510ff2a80b
Fernando Balladares <balladares.fernando.92@gmail.com>
2018-02-17 17:45:05 -0500

    limpieza

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,9 @@
   	<meta charset="utf-8">
   	<title>Administración</title>
     <link rel="StyleSheet" href="/assets/css/admin.css" type="text/css">
+    <link href="/assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image/png"  href="/assets/img/logo/icono1.png">
+    <!-- <link href="/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
   </head>
   <body>
 
--- pages/adminMenu.php
+++ pages/adminMenu.php
@@ -10,16 +10,16 @@
   <nav id="menuAdminCRUD" class="userMenu">
     <ul>
       <li><span><?php echo "USERNAME: " . $_SESSION['username']. " (admin)"; ?></span></li>
-      <li><a href='/admin/users'>Users</a></li>
-      <li><a href='/admin/articles'>Articles</a></li>
-      <li><a href='/admin/articles-comments'>Article_comments</a></li>
-      <li><a href='/admin/diseases'>Diseases</a></li>
+      <li><a href='/admin/users'><i class="fa fa-users"></i>Usuarios</a></li>
+      <li><a href='/admin/articles'><i class="fa fa-newspaper"></i>Artículos</a></li>
+      <li><a href='/admin/articles-comments'><i class="fa fa-comments"></i>Comentarios</a></li>
+      <li><a href='/admin/diseases'>Enfermedades</a></li>
       <li><a href='/admin/diseases-supporting-organizations'>Diseases Supporting Organizations</a></li>
-      <li><a href='/admin/supporting-organizations'>Supporting Organizations</a></li>
-      
+      <li><a href='/admin/supporting-organizations'><i class="fa fa-medkit"></i>Organizaciones</a></li>
 
-      <li><a href='/admin'>Volver a ADMIN</a></li>
-      <li><a href='/src/logout.php'>Salir</a></li>
+
+      <li><a href='/admin'><i class="fa fa-home"></i>Volver a ADMIN</a></li>
+      <li><a href='/src/logout.php'><i class="fa fa-sign-out-alt"></i>Salir</a></li>
     </ul>
   </nav>
 </div>
